b2d7ac25619a80028191ed650e85a38f666326e5
delighted <deborahdelighted5@gmail.com>
2026-03-13 03:10:28 +0100

    wip:Added slash route

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,19 @@ const app = express();
 
 app.use(express.json()); 
 app.use("/api/user", userRouter); 
-app.use("/api/transaction", transactionRouter); 
+app.use("/api/transaction", transactionRouter);
+app 
 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+    res.json({
+      message: "Alvative API is running 🚀"
+    });
+  });
 
 export default app;
 
